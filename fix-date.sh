5dd587bc713238b6e30f695fdd5b211d573da330
Mark Nichols <mark@zanshin.net>
2020-09-28 17:43:31 -0500

    More date refactoring
    Using sed the script now properly ensures all dates have two-digit
    month and day values.
    All times now have seconds

--- fix-date.sh
+++ fix-date.sh
@@ -8,13 +8,8 @@ set -o pipefail
 #
 
 # Date ideal: YYYY-MM-DDTHH:MM:SS-05:00
-ymd=$(echo "$1" | awk '{print $2}' | sed "s/\"//g")
-
-# Deal with singleton months (2020-9-30)
-ymd=$(echo "$ymd" | sed "s/-\([0-9]\)-/-0\1-/")
-
-# Deal with singleton days (2020-09-4)
-ymd=$(echo "$ymd" | sed "s/-\([0-9]\)/-0\1"/)
+# Find the YYYY-MM-DD date and fix singleton months and days
+ymd=$(echo "$1" | awk '{print $2}' | sed "s/\"//g" | sed "s/-\([0-9]\)-/-0\1-/" | sed "s/-\([0-9]\)$/-0\1/")
 
 # Get HH:MM, if they exist, produce HH:MM:SS regardless
 hm=$(echo "$1" | awk '{print $3}' | sed "s/\"//g")
@@ -25,5 +20,10 @@ else
 fi
 
 printf "date: %sT%s-05:00" "$ymd" "$hms"
+
+# Ought to work, but doesn't so fuck it
+# INPUT=$(awk '{$1=""; print}' | sed s"/^ //")
+# echo "INPUT: $INPUT"
+# printf "date: $(date +%s -d $INPUT '+%Y-%m-%dT%H:%M:%S-0500')"
 
 exit 0
