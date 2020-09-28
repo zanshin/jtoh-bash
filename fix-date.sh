276c97519e72722999476acfb525983bd6751aa7
Mark Nichols <mark@zanshin.net>
2020-09-28 16:02:42 -0500

    More date fixing
    Hugo, or I guess Go is picky about dates.
    - Ensured all times have seconds
    - Ensured that months are always two digits
    - Ensured that days are always two digits

--- fix-date.sh
+++ fix-date.sh
@@ -9,8 +9,21 @@ set -o pipefail
 
 # Date ideal: YYYY-MM-DDTHH:MM:SS-05:00
 ymd=$(echo "$1" | awk '{print $2}' | sed "s/\"//g")
-hms=$(echo "$1" | awk '{print $3}' | sed "s/\"//g")
-if [[ -z "$hms" ]]; then hms=$(date +'%H:%M:%S'); fi
+
+# Deal with singleton months (2020-9-30)
+ymd=$(echo "$ymd" | sed "s/-\([0-9]\)-/-0\1-/")
+
+# Deal with singleton days (2020-09-4)
+ymd=$(echo "$ymd" | sed "s/-\([0-9]\)/-0\1"/)
+
+# Get HH:MM, if they exist, produce HH:MM:SS regardless
+hm=$(echo "$1" | awk '{print $3}' | sed "s/\"//g")
+if [[ -z "$hm" ]]; then
+  hms=$(date +'%H:%M:%S')
+else
+  hms="$hm":"$(date +'%S')"
+fi
+
 printf "date: %sT%s-05:00" "$ymd" "$hms"
 
 exit 0
